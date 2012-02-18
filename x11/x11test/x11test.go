12d0047fdabc0f7616c4d2aceb66c3c4e6861ddb
John Asmuth <jasmuth@gmail.com>
2012-02-17 22:00:40 -0500

    modularized test

--- x11/x11test/x11test.go
+++ x11/x11test/x11test.go
@@ -17,114 +17,16 @@
 package main
 
 import (
-	"github.com/skelterjohn/go.wde.x11"
+	"github.com/skelterjohn/go.wde/x11"
 	"github.com/skelterjohn/go.wde"
-	"image/color"
-	"image/draw"
-	"time"
-	"fmt"
-	"sync"
+	"github.com/skelterjohn/go.wde/wdetest"
 )
 
+func wgen() (w wde.Window, err error) {
+	w, err = x11.NewWindow()
+	return
+}
+
 func main() {
-
-	var wg sync.WaitGroup
-	x := func() {
-		dw, err := x11.NewWindow()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		dw.SetTitle("hi!")
-		dw.SetSize(100, 100)
-		dw.Show()
-
-		events := dw.EventChan()
-
-		var s draw.Image = dw.Screen()
-
-		done := make(chan bool)
-
-		go func() {
-			for ei := range events {
-				switch e := ei.(type) {
-				case wde.MouseDownEvent:
-					println("md", e.X, e.Y, e.Which)
-					dw.Close()
-				case wde.MouseUpEvent:
-					println("mu", e.X, e.Y, e.Which)
-				case wde.MouseMovedEvent:
-					println("mv", e.X, e.Y)
-				case wde.MouseDraggedEvent:
-					println("mdr", e.X, e.Y, e.Which)
-				case wde.MouseEnteredEvent:
-					println("men", e.X, e.Y)
-				case wde.MouseExitedEvent:
-					println("mex", e.X, e.Y)
-				case wde.KeyDownEvent:
-					println("kd", e.Letter)
-				case wde.KeyUpEvent:
-					println("ku", e.Letter)
-				case wde.KeyTypedEvent:
-					println("kt", e.Letter)
-				case wde.CloseEvent:
-					println("close")
-				case wde.ResizeEvent:
-					println("resize")
-					s = dw.Screen()
-				}
-			}
-			println("end of events")
-			done <- true
-		}()
-
-		for i := 0; ; i++ {
-			for x := 0; x < 100; x++ {
-				for y := 0; y < 100; y++ {
-					var r uint8
-					if x > 50 {
-						r = 255
-					}
-					var g uint8
-					if y >= 50 {
-						g = 255
-					}
-					var b uint8
-					if y < 25 || y >= 75 {
-						b = 255
-					}
-					if i%2 == 1 {
-						r = 255 - r
-					}
-
-					if y > 90 {
-						r = 255
-						g = 255
-						b = 255
-					}
-
-					if x == y {
-						r = 100
-						g = 100
-						b = 100
-					}
-
-					s.Set(x, y, color.RGBA{r, g, b, 255})
-				}
-			}
-			dw.FlushImage()
-			select {
-			case <-time.After(1e9):
-			case <-done:
-				wg.Done()
-				return
-			}
-		}
-	}
-	wg.Add(1)
-	go x()
-	//wg.Add(1)
-	//go x()
-	println("done")
-	wg.Wait()
+	wdetest.Run(wgen)
 }
