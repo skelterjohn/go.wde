ca378045e5aa74e8de1b3c3796c45d50223c7d66
John Asmuth <jasmuth@gmail.com>
2012-04-15 20:49:30 -0400

    icons working

--- xgb/xgb.go
+++ xgb/xgb.go
@@ -62,7 +62,7 @@ func NewWindow(width, height int) (w *Window, err error) {
 	
 	w.events = make(chan interface{})
 
-	
+
 	go w.handleEvents()
 
 	return
@@ -71,16 +71,13 @@ func NewWindow(width, height int) (w *Window, err error) {
 func (w *Window) SetIcon(icon image.Image) {
 	width := icon.Bounds().Max.X - icon.Bounds().Min.X
 	height := icon.Bounds().Max.Y - icon.Bounds().Min.Y
-	data := make([]int, width*height*4)
+	data := make([]int, width*height)
 	for x:=0; x<width; x++ {
 		for y:=0; y<height; y++ {
 			i := x+y*width
 			c := icon.At(x, y)
 			r, g, b, a := c.RGBA()
-			data[i] = int(a)
-			data[i+1] = int(r)
-			data[i+2] = int(g)
-			data[i+3] = int(b)
+			data[i] = int(a+r<<8+g<<16+b<<24)
 		}
 	}
 	wmicon := ewmh.WmIcon{
