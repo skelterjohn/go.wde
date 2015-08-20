fb0664e600532d493984f1f6521481c5fa1cecd3
sqweek <sqweek@gmail.com>
2015-08-20 14:08:04 +0800

    cocoa: do all ops on main thread
    Doing all Cocoa work on the main thread appears to fix random crashes
    witnessed during startup.

--- cocoa/cocoa_darwin.go
+++ cocoa/cocoa_darwin.go
@@ -28,7 +28,6 @@ import (
 	"image"
 	"image/draw"
 	"runtime"
-	"sync"
 	"unsafe"
 )
 
@@ -90,7 +89,6 @@ func (im Image) CopyRGBA(src *image.RGBA, bounds image.Rectangle) {
 type Window struct {
 	cw       C.GMDWindow
 	im       Image
-	oplock   sync.Mutex
 	ec       chan interface{}
 
 	cursor   wde.Cursor // current cursor
@@ -98,41 +96,41 @@ type Window struct {
 }
 
 func NewWindow(width, height int) (w *Window, err error) {
-	cw := C.openWindow()
-	if cw == nil {
-		return nil, errors.New("couldn't allocate window (out of memory?)")
-	}
-	w = &Window{
-		cw: cw,
-	}
-	w.SetSize(width, height)
+	onMainThread(func() {
+		cw := C.openWindow()
+		if cw == nil {
+			err = errors.New("couldn't allocate window (out of memory?)")
+			return
+		}
+		w = &Window{
+			cw: cw,
+		}
+		w.SetSize(width, height)
+	})
 	return
 }
 
 func (w *Window) SetTitle(title string) {
-	w.oplock.Lock()
-	defer w.oplock.Unlock()
-
-	ctitle := C.CString(title)
-	defer C.free(unsafe.Pointer(ctitle))
-	C.setWindowTitle(w.cw, ctitle)
+	onMainThread(func() {
+		ctitle := C.CString(title)
+		defer C.free(unsafe.Pointer(ctitle))
+		C.setWindowTitle(w.cw, ctitle)
+	})
 }
 
 func (w *Window) SetSize(width, height int) {
-	w.oplock.Lock()
-	defer w.oplock.Unlock()
-
-	C.setWindowSize(w.cw, _Ctype_int(width), _Ctype_int(height))
+	onMainThread(func() {
+		C.setWindowSize(w.cw, _Ctype_int(width), _Ctype_int(height))
+	})
 }
 
 func (w *Window) Size() (width, height int) {
-	w.oplock.Lock()
-	defer w.oplock.Unlock()
-
-	var rw, rh _Ctype_int
-	C.getWindowSize(w.cw, &rw, &rh)
-	width = int(rw)
-	height = int(rh)
+	onMainThread(func() {
+		var rw, rh _Ctype_int
+		C.getWindowSize(w.cw, &rw, &rh)
+		width = int(rw)
+		height = int(rh)
+	})
 	return
 }
 
@@ -142,33 +140,25 @@ func (w *Window) LockSize(lock bool) {
 
 func (w *Window) Show() {
 	onMainThread(func() {
-		w.show_main()
+		C.showWindow(w.cw)
 	})
 }
 
-func (w *Window) show_main() {
-	w.oplock.Lock()
-	defer w.oplock.Unlock()
-
-	C.showWindow(w.cw) // must run on main thread
-}
-
 func (w *Window) resizeBuffer(width, height int) (im wde.Image) {
-	w.oplock.Lock()
-	defer w.oplock.Unlock()
+	onMainThread(func() {
+		ci := C.getWindowScreen(w.cw)
 
-	ci := C.getWindowScreen(w.cw)
+		w.im = Image{image.NewRGBA(image.Rectangle{
+			image.Point{},
+			image.Point{width, height},
+		})}
 
-	w.im = Image{image.NewRGBA(image.Rectangle{
-		image.Point{},
-		image.Point{width, height},
-	})}
+		ptr := unsafe.Pointer(&w.im.Pix[0])
 
-	ptr := unsafe.Pointer(&w.im.Pix[0])
+		C.setScreenData(ci, ptr)
 
-	C.setScreenData(ci, ptr)
-
-	im = w.im
+		im = w.im
+	})
 	return
 }
 
@@ -193,20 +183,18 @@ newbuffer:
 }
 
 func (w *Window) FlushImage(bounds ...image.Rectangle) {
-	w.oplock.Lock()
-	defer w.oplock.Unlock()
-
-	C.flushWindowScreen(w.cw)
+	onMainThread(func() {
+		C.flushWindowScreen(w.cw)
+	})
 }
 
 func (w *Window) Close() (err error) {
-	w.oplock.Lock()
-	defer w.oplock.Unlock()
-
-	ecode := C.closeWindow(w.cw)
-	if ecode != 0 {
-		err = errors.New(fmt.Sprintf("error:%d", ecode))
-	}
+	onMainThread(func() {
+		ecode := C.closeWindow(w.cw)
+		if ecode != 0 {
+			err = errors.New(fmt.Sprintf("error:%d", ecode))
+		}
+	})
 	return
 }
 
